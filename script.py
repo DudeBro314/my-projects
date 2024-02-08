b6b8fe220c4e8952c9d15c8891ccec6c49efd30f
DudeBro314 <155652680+DudeBro314@users.noreply.github.com>
2024-02-08 18:48:30 +0300

    Add files via upload
    This code is capable of encrypting a string with different registers using the ceasar cipher

--- script.py
+++ script.py
@@ -1,6 +1,22 @@
 alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-# I misspeled the word in C++ LOL
-Ceasar = lambda crypt, key, code=1: "".join([alph[(alph.find(char) + key * code) % 26] for char in crypt])
 
-print(Ceasar("TEST", 3))
+CheckIfChar = lambda char: char.isalpha()
+
+
+def MatchRegister(source, char):
+    if source.islower():
+        return char.lower()
+    return char
+
+
+def CeasarCore(line, key, direction=1):
+    crypt = ""
+    for char in line:
+        if CheckIfChar(char):
+            crypted_char = alph[(alph.find(char.upper()) + key * direction) % 26]
+            crypt += MatchRegister(char, crypted_char)
+        else:
+            crypt += char
+    return crypt
+
